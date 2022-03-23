15a141a129484ffa767071e7edecd07803b869ab
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2022-03-23 07:52:19 -0700

    chore: remove unnecessary file

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ extern crate native_windows_gui as nwg;
 use nwg::NativeUi;
 
 mod gui;
-mod modules;
+mod modules {
+    pub mod csv;
+    pub mod database;
+    pub mod utility;
+}
 use gui::BarTracker;
-use modules::database;
+use modules::database::create_new_log;
 
 fn main() {
-    database::create_new_log();
+    create_new_log();
     nwg::init().expect("Failed to init Native Windows GUI");
     let _app = BarTracker::build_ui(Default::default()).expect("Failed to build UI");
     nwg::dispatch_thread_events();
--- src/modules/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod csv;
-pub mod database;
-pub mod utility;
